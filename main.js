5e11b271fdbfa4ef03f27357fe3fed21709d412e
jayeshkausalamitron <163313351+jayeshkausalamitron@users.noreply.github.com>
2024-04-20 01:54:18 +0530

    latest code is uploaded

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
+const {dialog} = require('electron');
+const xlsx = require('xlsx');
 const { getToken, getReports } = require('./quickbooks');
 
 let mainWindow;
@@ -49,6 +52,21 @@ ipcMain.on('fetchReports', async (event, arg) => {
     const token = await getToken(); // Implement this function to get access token
     const reports = await getReports(token); // Implement this function to fetch reports
     mainWindow.webContents.send('reports', reports);
+    const workbook = xlsx.utils.book_new();
+    const worksheet = xlsx.utils.json_to_sheet(data);
+
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    //Changes this path as needed
+    const defaultPath = app.getPath('desktop');
+    const filePath = dialog.showSaveDialogSync(mainWindow, {
+        defaultPath,
+        filters: [{ name : 'Excek workbook', extensions: ['xlsx']}]
+    });
+
+    if(filePath){
+        xlsx.writeFile(workbook, filePath);
+    }
   } catch (error) {
     console.error('Error fetching reports:', error);
   }
